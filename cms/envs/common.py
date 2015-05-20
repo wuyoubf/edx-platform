a17f3d04598e7cd6e7c33e27111aa0b7cc36e374
Usman Khalid <2200617@gmail.com>
2015-05-20 17:14:05 +0500

    Performance optimizations and cache to keep the bookmarks info updated.
    The cache uses the bookmarks.XBlockCache model.
    
    TNL-1945

--- cms/envs/common.py
+++ cms/envs/common.py
@@ -800,6 +800,9 @@ INSTALLED_APPS = (
     # edX Proctoring
     'edx_proctoring',
 
+    # Bookmarks
+    'openedx.core.djangoapps.bookmarks',
+
     # programs support
     'openedx.core.djangoapps.programs',
 
--- lms/envs/common.py
+++ lms/envs/common.py
@@ -1997,6 +1997,7 @@ INSTALLED_APPS = (
     'xblock_django',
 
     # Bookmarks
+    'openedx.core.djangoapps.bookmarks',
     'bookmarks',
 
     # programs support
